fc29d81be10d9aea253b4a6148dbd04d9152534c
waqar <wnukedev@gmail.com>
2023-10-22 16:22:02 -0500

    End of section 12 Adding style

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -12,7 +13,11 @@ export class ProductDetailsComponent implements OnInit {
 
   product?: Product;
 
-  constructor(private shopService:ShopService , private acitvateRoute :ActivatedRoute){}
+  constructor(private shopService:ShopService , private acitvateRoute :ActivatedRoute,
+     private bcService : BreadcrumbService){
+      this.bcService.set('@productDetails', ' ')
+     }
+
   ngOnInit(): void {
     this.loadProduct();
   }
@@ -20,7 +25,10 @@ export class ProductDetailsComponent implements OnInit {
   loadProduct(){
     const id = this.acitvateRoute.snapshot.paramMap.get('id')
     if(id) this.shopService.getProduct(+id).subscribe({
-      next: product => this.product = product,
+      next: product => {
+        this.product = product,
+        this.bcService.set('@productDetails',product.name)
+      },
       error:error => console.log(error)
     });
   }
